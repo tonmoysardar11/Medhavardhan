56486cfb3c02edab123ba5545665b1e39f4bc9f4
Tonmoy Sardar <110713788+tonmoysardar11@users.noreply.github.com>
2023-02-03 10:46:41 +0530

    corporate page sample,home page done

--- index.js
+++
@@ -1,6 +0,0 @@
-const myModal = document.getElementById('myModal_1')
-const myInput = document.getElementById('corporate')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
