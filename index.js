03f75ea50ec9b855b9dea4117fb656b10d20963a
Tonmoy Sardar <110713788+tonmoysardar11@users.noreply.github.com>
2023-03-10 23:27:59 +0530

    pincode added with form validation

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+let proceed=()=> {
+    document.forms["google-sheet"]["Name"].value;
+    if(document.getElementById("name").value == null){
+        alert("Form must be filled out");
+    }else{
+        window.open('donate2.html','_self')
+    }
 
 
-
-let gateway= ()=>{
-    window.open('donate2.html','_self')
 }
